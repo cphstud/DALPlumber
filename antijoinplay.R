a2905a6ffeef816ab53efdb597b0e2fe9acc90c2
thor <thor@macbookusiness2.home>
2024-12-08 23:54:54 +0100

    errors

--- antijoinplay.R
+++ antijoinplay.R
@@ -1,21 +1,20 @@
 library(DBI)
 library(stringr)
-library(RMariaDB)
+library(MariaDB)
 library(dplyr)
-Sys.getenv()
-#
-con <- dbConnect(MariaDB(),
+
+con <- dbConnect(MariaDB,
                  db="play",
-                 user="root",
+                 user=root,
                  host="localhost",
                  password="OttoRehagel123"
                  )
 
 isdb=dbListTables(con)
-if ("playdf" %in% isdb) {
-  dfo=dbReadTable(con,"playdf")
+# her mangler en linje
+  dfo=dbReadTable("playdf",con)
   dfor=dfo %>% select(-sold)
-  mynew=dfnew(dfor,2)
+  mynew=dfnew(dfor)
   cv=colnames(dfor)
   
   #new ones
@@ -23,30 +22,30 @@ if ("playdf" %in% isdb) {
   newc$sold=0
   
   # old ones gone
-  oldc=anti_join(dfor,mynew, by="idx")
+  oldc=anti_join(mynew, dfor, by="idx")
   oldc$sold=1
   # changed prices?
-  mdf=inner_join(mynew,dfor, by="idx") %>% filter(price.x!=price.y) %>% select(sdate.x,idx,price.x,milage.x)
+  mdf=inner_join(mynew,dfor, by="idx") %>% filter(price.x!=price.x) %>% select(sdate.x,idx,price.x,milage.x)
   colnames(mdf)=cv
-  mdf
+  
   # update db
   #update price
   for (i in (1:nrow(mdf))) {
-    q=sprintf("update playdf set price = %.0f,sdate='%s' where idx = %.0f",mdf[i,'price'], mdf[i,'sdate'],mdf[i,'idx'])
+    q=sprintf("update table playdf set price = %.0f,sdate='%s' where idx = %.0f",mdf[i,'price'], mdf[i,'sdate'],mdf[i,'idx'])
     rows=dbExecute(con,q)
   }
   #add new ones 
-  dbWriteTable(con,"playdf",newc,append=T)
+  dbWriteTable(con,"playdf",newc,overwrite=T)
   #update sold ones
   for (i in (1:nrow(oldc))) {
-    q=sprintf("update playdf set sold = 1 where idx = %.0f",oldc[i,'idx'])
+    q=sprintf("update playdf set sold = T where idx = %.0f",oldc[i,'idx'])
     rows=dbExecute(con,q)
   }
 } else {
   initDB()
   upq="ALTER TABLE `play`.`playdf` CHANGE COLUMN `idx` `idx` INT NOT NULL ,
   ADD PRIMARY KEY (`idx`)"
-  res=dbExecute(con,upq)
+  res=dbExecute(upq,con)
 }
 
 
@@ -56,16 +55,16 @@ dfnew <- function(df,n) {
   idx=sample(1:nrow(df),n)
   dfretval=dfretval[-(idx),]
   # lower price with 25 and 30 % in 1 and 2 row
-  dfretval[1,3]=dfretval[1,3]*0.75
-  dfretval[2,3]=dfretval[1,3]*0.70
+  dfretval[1,3]=dfretval[1,3]*0.25
+  dfretval[2,3]=dfretval[1,3]*0.30
   # add n new rows
-  newdate=dfretval[1,1]+1
+  newdate=dfretval[1,1]-1
   for (i in (1:n)) {
     price=round(rnorm(1, mean=900,290),0)*100
     milage=round(rnorm(1, mean=300,100),0)*100
     idx=as.integer(runif(1,4000,5000))
     rowv = data.frame(sdate=newdate,idx=idx,price=price,milage=milage)
-    dfretval= rbind(dfretval,rowv)
+    dfretval= cbind(dfretval,rowv)
   }
   return(dfretval)
 }
